5be83f3c3a5bd3c790a8c3643f380dac538531fd
Tuopadre21 <vazzanaandrea5@gmail.com>
2024-05-30 15:41:48 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,330 @@
+function toggleText1() {
+    var text1Element = document.getElementById("text1");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+function toggleText2() {
+    var text2Element = document.getElementById("text2");
+    if (text2Element.classList.contains("hidden")) {
+        text2Element.classList.remove("hidden");
+} else {
+        text2Element.classList.add("hidden");
+}
+}
+
+
+function toggleText3() {
+    var text1Element = document.getElementById("text3");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+
+
+
+
+ function toggleText4() {
+    var text1Element = document.getElementById("text4");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+
+ function toggleText5() {
+    var text1Element = document.getElementById("text5");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText6() {
+    var text1Element = document.getElementById("text6");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText7() {
+    var text1Element = document.getElementById("text7");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText8() {
+    var text1Element = document.getElementById("text8");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText9() {
+    var text1Element = document.getElementById("text9");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText10() {
+    var text1Element = document.getElementById("text10");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText11() {
+    var text1Element = document.getElementById("text11");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText12() {
+    var text1Element = document.getElementById("text12");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText13() {
+    var text1Element = document.getElementById("text13");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText14() {
+    var text1Element = document.getElementById("text14");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText15() {
+    var text1Element = document.getElementById("text15");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText16() {
+    var text1Element = document.getElementById("text16");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText17() {
+    var text1Element = document.getElementById("text17");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText18() {
+    var text1Element = document.getElementById("text18");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText19() {
+    var text1Element = document.getElementById("text19");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText20() {
+    var text1Element = document.getElementById("text20");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText21() {
+    var text1Element = document.getElementById("text21");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText22() {
+    var text1Element = document.getElementById("text22");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText23() {
+    var text1Element = document.getElementById("text23");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText24() {
+    var text1Element = document.getElementById("text24");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText25() {
+    var text1Element = document.getElementById("text25");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText26() {
+    var text1Element = document.getElementById("text26");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText27() {
+    var text1Element = document.getElementById("text27");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText28() {
+    var text1Element = document.getElementById("text28");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText29() {
+    var text1Element = document.getElementById("text29");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText30() {
+    var text1Element = document.getElementById("text30");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText31() {
+    var text1Element = document.getElementById("text31");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText32() {
+    var text1Element = document.getElementById("text32");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText33() {
+    var text1Element = document.getElementById("text33");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+function toggleText34() {
+    var text1Element = document.getElementById("text34");
+    if (text1Element.classList.contains("hidden")) {
+        text1Element.classList.remove("hidden");
+    } else {
+        text1Element.classList.add("hidden");
+    }
+}
+
+
+
+
+
+
+
+
+
+function searchByID() {
+    var input = document.getElementById("searchInput").value; 
+    var section = document.getElementById("characters"); 
+    var targetElement = section.querySelector("#" + input); 
+
+    if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth" }); 
+    } else {
+        alert("ID non trovato all'interno della sezione"); 
+    }
+}
